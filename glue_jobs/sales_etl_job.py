4f719cc7ffb151cb00dd977aa583ac9d69e9d2e5
rajeshg <rajeshkhannagajulapati@gmail.com>
2026-02-20 14:35:04 +0530

    Enable Glue bookmarks for incremental processing

--- glue_jobs/sales_etl_job.py
+++ glue_jobs/sales_etl_job.py
@@ -1,26 +1,42 @@
-from pyspark.sql import SparkSession
+import sys
+from pyspark.context import SparkContext
 from pyspark.sql.functions import col, to_date
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
 
-spark = SparkSession.builder.getOrCreate()
+# Initialize Glue context
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
 
-# Read raw data from Glue Catalog
-raw_df = spark.sql("""
-    SELECT *
-    FROM sales_db.sales
-""")
+# Read from Glue Catalog (Bookmark enabled automatically)
+dynamic_frame = glueContext.create_dynamic_frame.from_catalog(
+    database="sales_db",
+    table_name="sales"
+)
+
+# Convert to Spark DataFrame
+df = dynamic_frame.toDF()
 
 # Transformations
 transformed_df = (
-    raw_df
+    df
     .filter(col("region") != "region")
     .withColumn("order_date", to_date(col("order_date"), "yyyy-MM-dd"))
     .withColumn("total_amount", col("quantity") * col("price"))
 )
-
-# Write processed data to S3
+    
+# Write to S3 in append mode
 (
     transformed_df.write
     .mode("append")
     .partitionBy("region")
     .parquet("s3://glue-sales-etl-rajesh/processed/sales/")
 )
+
+# Commit bookmark state
+job.commit()
